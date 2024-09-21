b5adc4a06ad83b312af7aedec024378021803b59
masfajaranugrah <massfajar223@gamil.com>
2024-09-21 23:12:13 +0700

    egc

---
+++ resources/views/Components/Header.blade.php
@@ -0,0 +1,286 @@
+    
+<!--==================== Preloader Start ====================-->
+ {{-- <div class="loader-mask">
+  <div class="loader">
+      <div></div>
+      <div></div>
+  </div>
+</div> --}}
+<!--==================== Preloader End ====================-->
+
+{{-- <!--==================== Overlay Start ====================-->
+<div class="overlay"></div>
+<!--==================== Overlay End ====================-->
+
+<!--==================== Sidebar Overlay End ====================-->
+<div class="side-overlay"></div>
+<!--==================== Sidebar Overlay End ====================-->
+
+<!-- ==================== Scroll to Top End Here ==================== -->
+<div class="progress-wrap">
+  <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+      <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+  </svg>
+</div> --}}
+<!-- ==================== Scroll to Top End Here ==================== -->
+
+<!-- ==================== Mobile Menu Start Here ==================== -->
+<div class="mobile-menu d-lg-none d-block">
+    <button type="button" class="close-button"> <i class="las la-times"></i> </button>
+    <div class="mobile-menu__inner">
+        <a href="index.html" class="mobile-menu__logo">
+            <img src="assets/images/logo/logo.png" alt="Logo" class="white-version">
+            <img src="assets/images/logo/white-logo-two.png" alt="Logo" class="dark-version">
+        </a>
+        <div class="mobile-menu__menu">
+             
+<ul class="nav-menu flx-align nav-menu--mobile">
+    <li class="nav-menu__item has-submenu">
+        <a href="javascript:void(0)" class="nav-menu__link">Home</a>
+        <ul class="nav-submenu">
+            <li class="nav-submenu__item">
+                <a href="index.html" class="nav-submenu__link"> Home One</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="index-two.html" class="nav-submenu__link"> Home Two</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="index-three.html" class="nav-submenu__link"> Home Three</a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-menu__item has-submenu">
+        <a href="javascript:void(0)" class="nav-menu__link">Products</a>
+         <ul class="nav-submenu">
+            <li class="nav-submenu__item">
+                <a href="all-product.html" class="nav-submenu__link"> All Products</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="product-details.html" class="nav-submenu__link"> Product Details</a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-menu__item has-submenu">
+        <a href="javascript:void(0)" class="nav-menu__link">Pages</a>
+         <ul class="nav-submenu">
+            <li class="nav-submenu__item">
+                <a href="profile.html" class="nav-submenu__link"> Profile</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="cart.html" class="nav-submenu__link"> Shopping Cart</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="cart-personal.html" class="nav-submenu__link"> Mailing Address</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="cart-payment.html" class="nav-submenu__link"> Payment Method</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="cart-thank-you.html" class="nav-submenu__link"> Preview Order</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="dashboard.html" class="nav-submenu__link"> Dashboard</a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-menu__item has-submenu">
+        <a href="javascript:void(0)" class="nav-menu__link">Blog</a>
+         <ul class="nav-submenu">
+            <li class="nav-submenu__item">
+                <a href="blog.html" class="nav-submenu__link"> Blog</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="blog-details.html" class="nav-submenu__link"> Blog Details</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="blog-details-sidebar.html" class="nav-submenu__link"> Blog Details Sidebar</a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-menu__item">
+        <a href="contact.html" class="nav-menu__link">Contact</a>
+    </li>
+</ul>
+            <div class="header-right__inner d-lg-none my-3 gap-1 d-flex flx-align">
+                
+    <a href="register.html" class="btn btn-main pill">
+        <span class="icon-left icon"> 
+            <img src="assets/images/icons/user.svg" alt="">
+        </span>Create Account  
+    </a>
+    <div class="language-select flx-align select-has-icon">
+        <img src="assets/images/icons/globe.svg" alt="" class="globe-icon white-version">
+        <img src="assets/images/icons/globe-white.svg" alt="" class="globe-icon dark-version">
+        <select class="select py-0 ps-2 border-0 fw-500">
+            <option value="1">Eng</option>
+            <option value="2">Bn</option>
+            <option value="3">Eur</option>
+            <option value="4">Urd</option>
+        </select>
+    </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ==================== Mobile Menu End Here ==================== -->
+
+{{-- <main class="change-gradient">
+    <!-- ============================ Sale Offer Start =========================== -->
+<div class="sale-offer ">
+    <div class="container container-full ">
+        <div class="sale-offer__content flx-between position-relative">
+            <div class="sale-offer__countdown">
+    
+                <div class="countdown" data-date="14-10-2026" data-time="12:00">
+                    <div class="day"><span class="num"></span><span class="word"></span></div>
+                    <div class="hour"><span class="num"></span><span class="word"></span></div>
+                    <div class="min"><span class="num"></span><span class="word"></span></div>
+                    <div class="sec"><span class="num"></span><span class="word"></span></div>
+                </div>
+    
+            </div>
+            <div class="sale-offer__discount flx-align gap-2">
+                <span class="sale-offer__text text-heading text-capitalize">New Year Flash Sale Offer</span>
+                <strong class="sale-offer__qty text-heading font-heading">45% OFF</strong>
+                <a href="#" class="btn btn-sm btn-white pill fw-500">Shop Now</a>
+            </div>
+            <div class="sale-offer__button">
+                <button type="submit" class="sale-offer__close text-heading"><i class="las la-times"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ============================ Sale Offer End =========================== --> --}}
+    <!-- ==================== Header Start Here ==================== -->
+<header class="header">
+    <div class="container container-full">
+        <nav class="header-inner flx-between">
+            <!-- Logo Start -->
+            <div class="logo">
+                <a href="index.html" class="link white-version">
+                    <img src="assets/images/logo/logo-two.png" alt="Logo">
+                </a>
+                <a href="index.html" class="link dark-version">
+                    <img src="assets/images/logo/white-logo.png" alt="Logo">
+                </a>
+            </div>
+            <!-- Logo End  -->
+
+            <!-- Menu Start  -->
+            <div class="header-menu d-lg-block d-none">
+                 
+<ul class="nav-menu flx-align ">
+    <li class="nav-menu__item has-submenu">
+        <a href="javascript:void(0)" class="nav-menu__link">Home</a>
+        <ul class="nav-submenu">
+            <li class="nav-submenu__item">
+                <a href="index.html" class="nav-submenu__link"> Home One</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="index-two.html" class="nav-submenu__link"> Home Two</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="index-three.html" class="nav-submenu__link"> Home Three</a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-menu__item has-submenu">
+        <a href="javascript:void(0)" class="nav-menu__link">Products</a>
+         <ul class="nav-submenu">
+            <li class="nav-submenu__item">
+                <a href="all-product.html" class="nav-submenu__link"> All Products</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="product-details.html" class="nav-submenu__link"> Product Details</a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-menu__item has-submenu">
+        <a href="javascript:void(0)" class="nav-menu__link">Pages</a>
+         <ul class="nav-submenu">
+            <li class="nav-submenu__item">
+                <a href="profile.html" class="nav-submenu__link"> Profile</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="cart.html" class="nav-submenu__link"> Shopping Cart</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="cart-personal.html" class="nav-submenu__link"> Mailing Address</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="cart-payment.html" class="nav-submenu__link"> Payment Method</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="cart-thank-you.html" class="nav-submenu__link"> Preview Order</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="dashboard.html" class="nav-submenu__link"> Dashboard</a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-menu__item has-submenu">
+        <a href="javascript:void(0)" class="nav-menu__link">Blog</a>
+         <ul class="nav-submenu">
+            <li class="nav-submenu__item">
+                <a href="blog.html" class="nav-submenu__link"> Blog</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="blog-details.html" class="nav-submenu__link"> Blog Details</a>
+            </li>
+            <li class="nav-submenu__item">
+                <a href="blog-details-sidebar.html" class="nav-submenu__link"> Blog Details Sidebar</a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-menu__item">
+        <a href="contact.html" class="nav-menu__link">Contact</a>
+    </li>
+</ul>
+            </div>
+            <!-- Menu End  -->
+
+            <!-- Header Right start -->
+            <div class="header-right flx-align">
+    <a href="cart.html" class="header-right__button cart-btn position-relative">
+        <img src="assets/images/icons/cart.svg" alt="" class="white-version">
+        <img src="assets/images/icons/cart-white.svg" alt="" class="dark-version">
+        <span class="qty-badge font-12">0</span>
+    </a>
+
+     <!-- Light Dark Mode -->
+ <div class="theme-switch-wrapper position-relative">
+    <label class="theme-switch" for="checkbox">
+        <input type="checkbox" class="d-none" id="checkbox">
+        <span class="slider text-black header-right__button white-version">
+            <i class="fa-solid fa-sun text-white"></i>        </span>
+        <span class="slider text-black header-right__button dark-version">
+            <i class="fa-solid fa-moon text-white"></i>
+        </span>
+    </label>
+</div>
+    
+    <div class="header-right__inner gap-3 flx-align d-lg-flex d-none">
+        
+    <a href="register.html" class="btn btn-main pill">
+        <span class="icon-left icon"> 
+            <img src="assets/images/icons/user.svg" alt="">
+        </span>Create Account  
+    </a>
+    <div class="language-select flx-align select-has-icon">
+        <img src="assets/images/icons/globe.svg" alt="" class="globe-icon white-version">
+        <img src="assets/images/icons/globe-white.svg" alt="" class="globe-icon dark-version">
+        <select class="select py-0 ps-2 border-0 fw-500">
+            <option value="1">Eng</option>
+            <option value="2">Bn</option>
+            <option value="3">Eur</option>
+            <option value="4">Urd</option>
+        </select>
+    </div>
+    </div>
+    <button type="button" class="toggle-mobileMenu d-lg-none"> <i class="las la-bars"></i> </button>
+</div>
+            <!-- Header Right End  -->
+        </nav>
+    </div>
+</header>
+<!-- ==================== Header End Here ==================== -->
---
+++ resources/views/Layouts/Home.blade.php
@@ -0,0 +1,242 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    <title> Digital Market Place HTML Template</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/logo/favicon-two.png">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <!-- Slick -->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <!-- magnific popup -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <!-- line awesome -->
+    <link rel="stylesheet" href="assets/css/line-awesome.min.css">
+    <!-- Main css -->
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/solid.min.css" integrity="sha512-/r+0SvLvMMSIf41xiuy19aNkXxI+3zb/BN8K9lnDDWI09VM0dwgTMzK7Qi5vv5macJ3VH4XZXr60ip7v13QnmQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    
+ 
+ 
+@include('Components.Header')
+ 
+@include('Components.Banner')
+ 
+@include('Components.Popular')
+ 
+@include('Components.Selling') 
+
+@include('Components.Project') 
+ 
+
+ 
+<section class="seller padding-y-120">
+    <div class="container container-two">
+        <div class="row gy-4">
+            <div class="col-lg-6">
+                <div class="seller-item position-relative z-index-1">
+                    <img src="assets/images/shapes/seller-bg.png" class="position-absolute
+                    start-0 top-0 z-index--1" alt="">
+                    <h3 class="seller-item__title">Earn 75% of the ItemD Price</h3>
+                    <p class="seller-item__desc fw-500 text-heading">Sellers receive 75% of the Itemp Price for items Dsold exclusively and 50% for items sold non-exclusively. See detailed informationabout the fee structure on Market.</p>
+                    <a href="register.html" class="btn btn-static-outline-black btn-xl pill fw-600">Become a Seller</a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="seller-item bg-two position-relative z-index-1">
+                    <img src="assets/images/shapes/seller-bg-two.png" class="position-absolute
+                    start-0 top-0 z-index--1" alt="">
+                    <h3 class="seller-item__title">Earn until 40% commission</h3>
+                    <p class="seller-item__desc fw-500 text-heading">Our Market is the world’s largest creative market place, selling millions of digital assets every year. With 30% affiliate commission, earning money has never been easier!</p>
+                    <a href="register.html" class="btn btn-static-outline-black btn-xl pill fw-600">Become an Affiliate</a>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="support position-relative z-index-1">
+                    <img src="assets/images/shapes/spider-net-sm.png" alt="" class="spider-net position-absolute top-0 end-0 z-index--1">
+                    <img src="assets/images/shapes/arrow-shape.png" alt="" class="arrow-shape">
+                    <div class="row align-items-center">
+                        <div class="col-lg-1 d-lg-block d-none"></div>
+                        <div class="col-lg-3 col-md-4 d-md-block d-none">
+                            <div class="support-thumb text-center">
+                                <img src="assets/images/thumbs/support-img.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 d-lg-block d-none"></div>
+                        <div class="col-lg-5 col-md-8">
+                            <div class="support-content">
+                                <h3 class="support-content__title mb-3">Support 24/7</h3>
+                                <p class="support-content__desc">Wanna talk? Send us a message</p>
+                                <a href="mailto:infomail@office.com" class="btn btn-static-black btn-lg fw-300 pill">infomail@office.com</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ======================= Become seller section End ==================== -->
+    
+    <!-- ======================== Brand Section Start ========================= -->
+<div class="brand ">
+    <div class="container container">
+        <div class="brand-slider">
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img1.png" alt="" class="white-version">
+                <img src="assets/images/thumbs/brand-white-img1.png" alt="" class="dark-version">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img2.png" alt="" class="white-version">
+                <img src="assets/images/thumbs/brand-white-img2.png" alt="" class="dark-version">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img3.png" alt="" class="white-version">
+                <img src="assets/images/thumbs/brand-white-img3.png" alt="" class="dark-version">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img4.png" alt="" class="white-version">
+                <img src="assets/images/thumbs/brand-white-img4.png" alt="" class="dark-version">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img5.png" alt="" class="white-version">
+                <img src="assets/images/thumbs/brand-white-img5.png" alt="" class="dark-version">
+            </div>
+            <div class="brand-item d-flex align-items-center justify-content-center">
+                <img src="assets/images/thumbs/brand-img3.png" alt="" class="white-version">
+                <img src="assets/images/thumbs/brand-white-img3.png" alt="" class="dark-version">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ======================== Brand Section End ========================= -->
+    
+<!-- ==================== Footer Start Here ==================== -->
+<footer class="footer-section ">
+    <img src="assets/images/shapes/pattern.png" alt="" class="bg-pattern">
+    <img src="assets/images/shapes/element1.png" alt="" class="element one">
+    <img src="assets/images/shapes/element2.png" alt="" class="element two">
+    <img src="assets/images/gradients/footer-gradient.png" alt="" class="bg--gradient">
+
+    <div class="container container-two">
+        <div class="row gy-5">
+            <div class="col-xl-3 col-sm-6">
+                <div class="footer-widget">
+                    <div class="footer-widget__logo">
+                        <a href="index.html"> <img src="assets/images/logo/white-logo.png" alt=""></a>
+                    </div>
+                    <p class="footer-widget__desc">Lorem consultancy elitsed do eiusmod tempor inci didunt ut labore dolore magna aliqua sed do eiusmod.</p>                    
+                    <div class="footer-widget__social">
+                        <ul class="social-icon-list">
+    <li class="social-icon-list__item">
+        <a href="https://www.facebook.com" class="social-icon-list__link flx-center"><i class="fab fa-facebook-f"></i></a>
+    </li>
+    <li class="social-icon-list__item">
+        <a href="https://www.twitter.com" class="social-icon-list__link flx-center"> <i class="fab fa-twitter"></i></a>
+    </li>
+    <li class="social-icon-list__item">
+        <a href="https://www.linkedin.com" class="social-icon-list__link flx-center"> <i class="fab fa-linkedin-in"></i></a>
+    </li>
+    <li class="social-icon-list__item">
+        <a href="https://www.pinterest.com" class="social-icon-list__link flx-center"> <i class="fab fa-pinterest-p"></i></a>
+    </li>
+    <li class="social-icon-list__item">
+        <a href="https://www.pinterest.com" class="social-icon-list__link flx-center"> <i class="fab fa-youtube"></i></a>
+    </li>
+</ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-2 col-sm-6 col-xs-6">
+                <div class="footer-widget">
+                    <h5 class="footer-widget__title text-white">Useful Link</h5>
+                    <ul class="footer-lists">
+                        <li class="footer-lists__item"><a href="all-product.html" class="footer-lists__link">Product </a></li>
+                        <li class="footer-lists__item"><a href="product-details.html" class="footer-lists__link">Product Details</a></li>
+                        <li class="footer-lists__item"><a href="profile.html" class="footer-lists__link">Profile </a></li>
+                        <li class="footer-lists__item"><a href="cart.html" class="footer-lists__link">Shopping Cart</a></li>
+                        <li class="footer-lists__item"><a href="dashboard.html" class="footer-lists__link">Dashboard</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 col-xs-6 ps-xl-5">
+                <div class="footer-widget">
+                    <h5 class="footer-widget__title text-white">Quick Links</h5>
+                    <ul class="footer-lists">
+                        <li class="footer-lists__item"><a href="dashboard.html" class="footer-lists__link">Dashboard </a></li>
+                        <li class="footer-lists__item"><a href="login.html" class="footer-lists__link">Login </a></li>
+                        <li class="footer-lists__item"><a href="register.html" class="footer-lists__link">Register</a></li>
+                        <li class="footer-lists__item"><a href="blog.html" class="footer-lists__link">Blog </a></li>
+                        <li class="footer-lists__item"><a href="blog-details.html" class="footer-lists__link">Blog Details</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-4 col-sm-6">
+                <div class="footer-widget">
+                    <h5 class="footer-widget__title text-white">Subscribe</h5>
+                    <p class="footer-widget__desc">Subscribe our newsletter to get updated the latest news</p>
+                    <form action="#" class="mt-4 subscribe-box d-flex align-items-center flex-column gap-2">
+                        <input type="text" class="form-control common-input pill text-white" placeholder="Enter Mail">
+                        <button type="submit" class="btn btn-main btn-lg w-100 pill">Subscribe Now</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- bottom Footer -->
+<div class="bottom-footer">
+    <div class="container container-two">
+        <div class="bottom-footer__inner flx-between gap-3">
+            <p class="bottom-footer__text font-14"> Copyright &copy; 2024 DPmarket, All rights reserved.</p>
+            <div class="footer-links">
+                <a href="#" class="footer-link font-14">Terms of service</a>
+                <a href="#" class="footer-link font-14">Privacy Policy</a>
+                <a href="contact.html" class="footer-link font-14">cookies</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ==================== Footer End Here ==================== -->
+  
+</main>
+
+    <!-- Jquery js -->
+    {{-- <script src="assets/js/jquery-3.7.1.min.js"></script> --}}
+    <script
+  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous"></script>
+
+    <!-- Bootstrap Bundle Js -->
+    <script src="assets/js/boostrap.bundle.min.js"></script>
+    <!-- CountDown -->
+    <script src="assets/js/countdown.js"></script>
+    <!-- counter up -->
+    <script src="assets/js/counterup.min.js"></script>
+    <!-- Slick js -->
+    <script src="assets/js/slick.min.js"></script>
+    <!-- magnific popup -->
+    {{-- <script src="assets/js/jquery.magnific-popup.js"></script> --}}
+    <!-- apex chart -->
+    <script src="assets/js/apexchart.js"></script>
+    <!-- marquee -->
+    <script src="assets/js/marquee.min.js"></script>
+
+    <!-- main js -->
+    <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
